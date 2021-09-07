81c9ad4302867bd3d722864db72f3813e2f15581
Youssef <youssefakanni2003@gmail.com>
2021-09-07 15:55:36 +0200

    2.6

---
+++ 2.6.js
@@ -0,0 +1,27 @@
+let day = prompt("Type a number between 1 and 7.");
+
+switch (day) {
+  case "1":
+    document.write("Monday");
+    break;
+  case "2":
+    document.write("Tuesday");
+    break;
+  case "3":
+    document.write("Wedesday");
+    break;
+  case "4":
+    document.write("Thursday");
+    break;
+  case "5":
+    document.write("Friday");
+    break;
+  case "6":
+    document.write("Saturday");
+    break;
+  case "7":
+    document.write("Sunday");
+    break;
+  default:
+    document.write("Number between 1 and 7");
+}
